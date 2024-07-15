20df4c722121de9e7b163b17b2ea77437912aa21
rahulgaikwadfinoux <124568232+rahulgaikwadfinoux@users.noreply.github.com>
2024-07-15 15:12:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,15 +41,30 @@ app.get('/', (req, res) => {
 
 app.get('/.well-known/assetlinks.json', (req, res) => {
   res.send(
-    [{
-      "relation": ["delegate_permission/common.handle_all_urls"],
-      "target": {
-        "namespace": "android_app", "package_name": "com.alramz.alramz",
-        "sha256_cert_fingerprints": ["FC:9E:EA:FE:41:2C:68:C8:76:13:26:2C:86:05:A0:BE:1E:26:03:83:30:97:B5:5F:FF:FC:01:98:CB:73:5B:59"]
-      }
-    }]
+    [
+  {
+    relation: ["delegate_permission/common.handle_all_urls"],
+
+    target: {
+      namespace: "android_app",
+      package_name: "com.alramz.alramz.uat",
+      sha256_cert_fingerprints: [
+        "81:12:FB:5A:64:E0:D7:EF:26:C3:73:C5:64:C4:4F:85:AB:96:D1:F8:D8:64:DB:6B:74:0C:40:1A:3C:B4:C0:45",
+      ],
+    },
+  },
+]
   );
 });
+
+// [{
+//   "relation": ["delegate_permission/common.handle_all_urls"],
+//   "target": {
+//     "namespace": "android_app", "package_name": "com.alramz.alramz",
+//     "sha256_cert_fingerprints": ["FC:9E:EA:FE:41:2C:68:C8:76:13:26:2C:86:05:A0:BE:1E:26:03:83:30:97:B5:5F:FF:FC:01:98:CB:73:5B:59"]
+//   }
+// }]
+
 // [
 //   {
 //     "relation": [
